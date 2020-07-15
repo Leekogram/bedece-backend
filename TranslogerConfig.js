95ffeb702d34fe66d58c646394dca8eff9a41cfa
Muhammad Aliyu <33817077+Leekogram@users.noreply.github.com>
2020-07-15 12:34:08 +0100

    changed db link

--- TranslogerConfig.js
+++ TranslogerConfig.js
@@ -15,7 +15,8 @@ const TransLog = createLogger({
 
         new transports.MongoDB({
             level:'info',
-            db: "mongodb+srv://sayil:SEYILNEN2194@cluster0-0j8cs.mongodb.net/test?retryWrites=true&w=majority",
+           // db: "mongodb+srv://sayil:SEYILNEN2194@cluster0-0j8cs.mongodb.net/test?retryWrites=true&w=majority",
+            db: "mongodb+srv://Jinn:jinnpassword@313bdccluster.x64qt.mongodb.net/313bdcCluster?retryWrites=true&w=majority",
             collection:'TransLogs',
             format: format.combine(format.timestamp(), format.json())
         })
