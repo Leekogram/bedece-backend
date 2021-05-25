e53c4ef78ce0dabba5a209896f1376ec4724a8b4
sayil1 <sundaysayil4uWgmail.com>
2021-05-25 07:08:34 +0100

    fix minor issues

--- models/purchase/transLogsModels.js
+++ models/purchase/transLogsModels.js
@@ -28,7 +28,7 @@ let TransSchema = new Schema({
         },
         refference: {
             type: String,
-            // default: `BCD/${new Date().toLocaleDateString()}/${customId}`,
+            default: `BCD/${new Date().toLocaleDateString()}/${customId}`,
             // once: true
             
         }
