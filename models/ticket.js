0687fbe775d4ee3da6344a7a1c8e04759345eee8
sayil1 <sundaysayil4uWgmail.com>
2021-06-05 08:55:40 +0100

    remove enum values for tickets:

--- models/ticket.js
+++ models/ticket.js
@@ -1,43 +1,60 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 let TicketSchema = new Schema({
-    subject: {
-        type: String
-    },
-    priority: {
-        type: String, enum: ["Low", "Medium", "High"], required: [true, "Has to be either one of these enum values: Low, Medium, High"]
-    },
-    type: {
-        type: String, enum: ["Technical", "Account"], required: [true, "Has to be either one of these enum values: Technical, Account"]
-    },
-    message: [{
-        sender: {
-            type: String, enum: ["admin", "user"], required: [true, "Please add the sender details - Must be of enum [admin or user]"]
-        },
-        comment: {
-            type: String
-        },
-       
-    }],
-    userId: {
-        type: Schema.Types.ObjectId,
-        ref: "RegModel",
-        required: [true, "User id is needed, i.e the sender"]
-    },
-    active: {
-        type: Boolean, default: true
-    },
-    
-    created_date: {
+  subject: {
+    type: String,
+  },
+  priority: {
+    type: String,
+    enum: ["Low", "Medium", "High"],
+    required: [
+      true,
+      "Has to be either one of these enum values: Low, Medium, High",
+    ],
+  },
+  type: {
+    type: String,
+    enum: ["Technical", "Account"],
+    required: [
+      true,
+      "Has to be either one of these enum values: Technical, Account",
+    ],
+  },
+  message: [
+    {
+      sender: {
+        type: String,
+        required: [true, "Please add the sender details"],
+      },
+      comment: {
+        type: String,
+      },
+      time: {
         type: Date,
         default: Date.now,
-        once: true
+        once: true,
+      },
     },
-    updated: {
-        type: Date,
-        default: Date.now,
+  ],
+  userId: {
+    type: Schema.Types.ObjectId,
+    ref: "RegModel",
+    required: [true, "User id is needed, i.e the sender"],
+  },
+  active: {
+    type: Boolean,
+    default: true,
+  },
 
-    }
-})
-const TicketModel = mongoose.model('TicketModel', TicketSchema)
+  created_date: {
+    type: Date,
+    default: Date.now,
+    once: true,
+  },
+  updated: {
+    type: Date,
+    default: Date.now,
+  },
+});
+const TicketModel = mongoose.model("TicketModel", TicketSchema);
 module.exports = TicketModel;
